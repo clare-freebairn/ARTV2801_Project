5489564d3a67a8c368e7f5633102363f68ed435b
Clarabelle <clellenf@gmail.com>
2020-10-07 12:28:14 +1100

    started Minigame 3, spelling game

--- Clare.js
+++ Clare.js
@@ -1,1 +1,9 @@
+var questionsMinigame3 = ["It's not ________ correct", "____ you be able to ____ this", "Does this ___ make it easier", ""];
 
+var answersMinigame3 = [["Necesarily", "Nessicarily"], ["Wuld/read", "Would/reed"], ["Fant", "Coler", "Sise"], ["", ""]];
+
+
+function spellingGame() {
+
+    
+}
